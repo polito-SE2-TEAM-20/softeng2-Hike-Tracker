480f03d6d91f0ba58ebee7e7449e444822aaf945
MrR0b0t14 <s297925@studenti.polito.it>
2022-11-11 20:24:59 +0100

    Task in verification and Code Reviewing

--- backend/src/hikes/hikes.controller.spec.ts
+++ backend/src/hikes/hikes.controller.spec.ts
@@ -13,7 +13,33 @@ describe('HikesController', () => {
     controller = module.get<HikesController>(HikesController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('Should return 1 hike in Salerno', async () => {
+    const body = {
+      "province": "Salerno",
+      "region": null,
+      "maxLength": 15,
+      "minLength": 5,
+      "expectedTimeMin": null,
+      "expectedTimeMax": null,
+      "difficultyMin": null,
+      "difficultyMax": null,
+      "ascentMin": null,
+      "ascentMax": null
+    };
+    const result = [{
+      "id": 4,
+      "userId": 0,
+      "length": 8.7,
+      "expectedTime": 200,
+      "ascent": 1200,
+      "difficulty": 0,
+      "title": "Te lo sfonni poco",
+      "description": "Hike poco impegnativo",
+      "gpxPath": null,
+      "region": "Campania",
+      "province": "Salerno"
+    }];
+
+    expect(controller.getFilteredHikes(body)).toBe(result);
   });
 });
--- backend/src/hikes/hikes.controller.ts
+++ backend/src/hikes/hikes.controller.ts
@@ -14,39 +14,30 @@ export class HikesController {
 
   @Post("/filteredHikes")
   async getFilteredHikes(@Body() body: any): Promise<Hike[]> {
-     let hikes = await this.dataSource.getRepository(Hike)
-                .createQueryBuilder('hikes')
-                .where(body.province !== null 
-                  ? 'hikes.province = :province'
-                  : 'TRUE', { province: body.province })
-                .andWhere(body.region !== null 
-                  ? 'hikes.region = :region'
-                  : 'TRUE', { region: body.region })
-                .andWhere(body.maxLength !== null 
-                  ? 'hikes.length <= :maxLength'
-                  : 'TRUE', { maxLength: body.maxLength })
-                .andWhere(body.minLength !== null 
-                  ? 'hikes.length >= :minLength'
-                  : 'TRUE', { minLength: body.minLength })
-                .andWhere(body.difficultyMax !== null 
-                  ? 'hikes.difficulty <= :maxDifficulty'
-                  : 'TRUE', { maxDifficulty: body.difficultyMax })
-                .andWhere(body.difficultyMin !== null 
-                  ? 'hikes.difficulty >= :difficultyMin'
-                  : 'TRUE', { difficultyMin: body.difficultyMin }) //ok
-                .andWhere(body.expectedTimeMax !== null 
-                  ? 'hikes.length <= :expectedTimeMax'
-                  : 'TRUE', { expectedTimeMax: body.expectedTimeMax })
-                .andWhere(body.expectedTimeMin !== null 
-                  ? 'hikes.expectedTime >= :expectedTimeMin'
-                  : 'TRUE', { expectedTimeMin: body.expectedTimeMin }) //ok
-                .andWhere(body.ascentMax !== null 
-                  ? 'hikes.ascent <= :ascentMax'
-                  : 'TRUE', { ascentMax: body.ascentMax })
-                .andWhere(body.ascentMin !== null 
-                  ? 'hikes.ascent >= :ascentMin'
-                  : 'TRUE', { ascentMin: body.ascentMin })
-                .getMany();
+    const query = this.dataSource.getRepository(Hike).createQueryBuilder('hikes');
+                
+    if(body.region !== null) 
+      query.where('hikes.region = :region', { region: body.region })
+    if(body.province !== null) 
+      query.andWhere('hikes.province = :province', { province: body.province })
+    if(body.maxLength !== null) 
+      query.andWhere('hikes.length <= :maxLength', { maxLength: body.maxLength })
+    if(body.minLength !== null) 
+      query.andWhere('hikes.length >= :minLength', { minLength: body.minLength })
+    if(body.difficultyMax !== null) 
+      query.andWhere('hikes.difficulty <= :maxDifficulty', { maxDifficulty: body.difficultyMax })
+    if(body.difficultyMin !== null) 
+      query.andWhere('hikes.difficulty >= :difficultyMin', { difficultyMin: body.difficultyMin })
+    if(body.expectedTimeMax !== null) 
+      query.andWhere('hikes.length <= :expectedTimeMax', { expectedTimeMax: body.expectedTimeMax })
+    if(body.expectedTimeMin !== null) 
+      query.andWhere('hikes.expectedTime >= :expectedTimeMin', { expectedTimeMin: body.expectedTimeMin })
+    if(body.ascentMax !== null) 
+      query.andWhere('hikes.ascent <= :ascentMax', { ascentMax: body.ascentMax })
+    if(body.ascentMin !== null) 
+      query.andWhere('hikes.ascent >= :ascentMin', { ascentMin: body.ascentMin })
+                
+    let hikes = await query.getMany();
     console.log(hikes); 
     return hikes;
   }
