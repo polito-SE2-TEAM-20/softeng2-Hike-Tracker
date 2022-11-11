84272cc54d87d322239850ed0536c06134764886
MrR0b0t14 <s297925@studenti.polito.it>
2022-11-11 20:24:34 +0100

    Task in verification and Code Reviewing

--- backend/src/hikes/hikes.controller.ts
+++ backend/src/hikes/hikes.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 
 import { Hike } from '@app/common';
@@ -10,5 +10,44 @@ export class HikesController {
   @Get()
   async getHikes(): Promise<Hike[]> {
     return await this.dataSource.getRepository(Hike).findBy({});
+  }
+
+  @Post("/filteredHikes")
+  async getFilteredHikes(@Body() body: any): Promise<Hike[]> {
+     let hikes = await this.dataSource.getRepository(Hike)
+                .createQueryBuilder('hikes')
+                .where(body.province !== null 
+                  ? 'hikes.province = :province'
+                  : 'TRUE', { province: body.province })
+                .andWhere(body.region !== null 
+                  ? 'hikes.region = :region'
+                  : 'TRUE', { region: body.region })
+                .andWhere(body.maxLength !== null 
+                  ? 'hikes.length <= :maxLength'
+                  : 'TRUE', { maxLength: body.maxLength })
+                .andWhere(body.minLength !== null 
+                  ? 'hikes.length >= :minLength'
+                  : 'TRUE', { minLength: body.minLength })
+                .andWhere(body.difficultyMax !== null 
+                  ? 'hikes.difficulty <= :maxDifficulty'
+                  : 'TRUE', { maxDifficulty: body.difficultyMax })
+                .andWhere(body.difficultyMin !== null 
+                  ? 'hikes.difficulty >= :difficultyMin'
+                  : 'TRUE', { difficultyMin: body.difficultyMin }) //ok
+                .andWhere(body.expectedTimeMax !== null 
+                  ? 'hikes.length <= :expectedTimeMax'
+                  : 'TRUE', { expectedTimeMax: body.expectedTimeMax })
+                .andWhere(body.expectedTimeMin !== null 
+                  ? 'hikes.expectedTime >= :expectedTimeMin'
+                  : 'TRUE', { expectedTimeMin: body.expectedTimeMin }) //ok
+                .andWhere(body.ascentMax !== null 
+                  ? 'hikes.ascent <= :ascentMax'
+                  : 'TRUE', { ascentMax: body.ascentMax })
+                .andWhere(body.ascentMin !== null 
+                  ? 'hikes.ascent >= :ascentMin'
+                  : 'TRUE', { ascentMin: body.ascentMin })
+                .getMany();
+    console.log(hikes); 
+    return hikes;
   }
 }
