82671b99fe11fb9f36c81ba3dee89984cc23a77a
FRANCESCO GRANDE s296962 <s296962@studenti.polito.it>
2022-11-13 19:09:16 +0100

    adding test

---
+++ backend/src/auth/auth.controller.spec.ts
@@ -0,0 +1,61 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { AuthController } from './auth.controller';
+
+describe('HikesController', () => {
+  let controller: AuthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+    }).compile();
+
+    controller = module.get<AuthController>(AuthController);
+  });
+
+  it('Should return registered user data', async () => {
+    const body = {
+        
+        email: "utente@prova.it",
+        firstName: "Utente",
+        lastName: "Di Prova",
+        password: "password",
+        role: 0
+        
+    };
+    const result = [
+        {
+            email: "utente@prova.it",
+            firstName: "Utente",
+            lastName: "Di Prova",
+            role: 0,
+            id: "1"
+        }
+    ];
+
+    expect(controller.register(body)).toBe(result);
+  });
+
+  it('Should return 1 token', async () => {
+    const body = {
+        
+        email: "utente@prova.it",
+        password: "password"
+        
+    };
+    const result = [
+        {
+            user: {
+                id: 1,
+                email: "utente@prova.it",
+                firstName: "Utente",
+                lastName: "Di Prova",
+                role: 0
+            },
+            token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY4MzYyNDQ0fQ.Iijf5rzdqQhXjE8-oSB5EzFL7IkAo1gMvbO7Ug3LXbQ"
+        }
+    ];
+
+    expect(controller.login(body)).toBe(result);
+  });
+});
