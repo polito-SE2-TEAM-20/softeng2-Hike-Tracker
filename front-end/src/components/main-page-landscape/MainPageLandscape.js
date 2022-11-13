e1e580d3714fd1b42acf6fce1ec6b67a8936c79d
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-11-13 15:04:22 +0100

    started: main page

--- front-end/src/components/main-page-landscape/MainPageLandscape.js
+++ front-end/src/components/main-page-landscape/MainPageLandscape.js
@@ -1,17 +1,19 @@
+import { Col, Row } from 'react-bootstrap'
 import './mpl-style.css'
 
 const MainPageLandscape = () => {
     return (
-        <div className="group-8">
-            <img
-                src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/h5sikuitbl-1%3A25?alt=media&token=5f05414a-7ba8-4752-ab01-31b2f68151c2"
-                alt="Not Found"
-                className="group-7"
-            />
-            <p className="txt-931">
-                where<span className="txt-9312"> will your next adventure be?</span>
-            </p>
-        </div>
+        <div style={{ display: "flex", justifyContent: "center", backgroundColor: "#bababa" }}>
+            <Row style={{ marginTop: "120px", marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}>
+                <p className="txt-931" style={{
+                    width: "1000px", color: "#1a1a1a", paddingBottom: "25px",
+                    margin: "auto", display: "flex", justifyContent: "center",
+                    borderRadius: "40px"
+                }}>
+                    where will your next adventure be?
+                </p>
+            </Row>
+        </div >
     )
 }
 
--- front-end/src/routes/main-page/MainPage.js
+++ front-end/src/routes/main-page/MainPage.js
@@ -3,16 +3,21 @@ import Button from '../../components/buttons/Button';
 import { Row, Container } from 'react-bootstrap'
 import Navbar from 'react-bootstrap/Navbar';
 import { useNavigate } from "react-router-dom";
+import MainPageLandscape from '../../components/main-page-landscape/MainPageLandscape';
+
 
 const MainPage = () => {
     return (
-        <Container fluid style={{ paddingLeft: "0px", paddingRight: "0px"}}>
+        <Container fluid style={{ paddingLeft: "0px", paddingRight: "0px" }}>
             <Navbar className="is-sticky" expand="lg">
                 <Container>
                     <MainTitle color="white" size="48px" />
                     <Button text="Login" textColor="black" color="white" size="24px" />
                 </Container>
             </Navbar>
+            <Row>
+                <MainPageLandscape />
+            </Row>
         </Container >
     );
 }
