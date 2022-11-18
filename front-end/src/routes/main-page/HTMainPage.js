ecf750c8b63c15c67dc7a3d2c4f7f0a895c6a352
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-11-18 14:28:09 +0100

    main page now is responsive

--- front-end/src/routes/main-page/HTMainPage.js
+++ front-end/src/routes/main-page/HTMainPage.js
@@ -2,6 +2,9 @@ import { useNavigate } from "react-router-dom";
 import { Grid } from '@mui/material';
 import { Typography } from '@mui/material';
 import HTNavbar from '../../components/HTNavbar/HTNavbar'
+import './main-page-style.css'
+import { Box } from "@mui/material";
+import { CardMedia } from "@mui/material";
 
 const HTMainPage = (props) => {
     const navigate = useNavigate()
@@ -11,29 +14,17 @@ const HTMainPage = (props) => {
     const gotoLogin = () => {
         navigate("/login", { replace: false })
     }
-    
+
     return (
-        <Grid container spacing={0} style={{ backgroundColor: "#A6A6A6", height: "100%", minHeight: "100vh" }}>
+        <Grid container spacing={0} style={{ backgroundColor: "#ffffff", height: "100%", minHeight: "100vh" }}>
             <HTNavbar isLoggedIn={props.isLoggedIn} doLogOut={props.doLogOut} gotoLogin={gotoLogin} navigate={props.navigate} />
-            <Grid display={responsiveContentStyleBig}
-                justifyContent="center"
-                item xs={12} sm={12} md={12} lg={12} xl={12}>
-                <img
-                    src={"https://cdn.pixabay.com/photo/2019/06/08/17/40/landscape-4260630_960_720.jpg"}
-                    alt="main-page-image"
-                    style={{ borderRadius: "62px", marginTop: "120px" }}
-                    loading="lazy"
-                />
-            </Grid>
-            <Grid display={responsiveContentStyleEnd} justifyContent="center" item xs={12} sm={12} md={12} lg={12} xl={12}>
-                <img
-                    src={"https://cdn.pixabay.com/photo/2019/06/08/17/40/landscape-4260630_960_720.jpg"}
-                    alt="main-page-image"
-                    style={{ borderRadius: "62px", heigth: "auto", width: { xs: "415px", lg: "500px" }, marginTop: "120px" }}
-                    loading="lazy"
-                />
-            </Grid>
-            <Grid item xs={12}>
+            <div style={{display: "grid", gridTemplateRows:"repeat(5, 1fr)", width: "100%"}}>
+                <CardMedia display={responsiveContentStyleBig} component="img"
+                    height="540px"
+                    style={{gridRow: "1/2"}}
+                    className="main-page"
+                    alt="Paella dish">
+                </CardMedia>
                 <Typography
                     variant="h2"
                     noWrap className="unselectable"
@@ -44,10 +35,14 @@ const HTMainPage = (props) => {
                         textAlign: "center",
                         color: '#1a1a1a',
                         textDecoration: 'none',
+                        gridRow: "2/2"
                     }}
                 >
                     where will your next adventure be?
                 </Typography>
+            </div>
+            <Grid item xs={12}>
+                
                 <Typography
                     variant="h4"
                     noWrap className="unselectable"
