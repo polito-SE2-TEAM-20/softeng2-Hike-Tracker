2fe081191f35c563c4ec866475420667143b4f24
German Gorodnev <germangorodnev@gmail.com>
2022-11-26 00:57:37 +0100

    finish docker-compose setup: healthchecks, depends_on

--- backend/src/healthcheck/healthcheck.controller.ts
+++ backend/src/healthcheck/healthcheck.controller.ts
@@ -1,9 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+
+import { User } from '@app/common';
 
 @Controller()
 export class HealthcheckController {
+  constructor(private dataSource: DataSource) {}
+
   @Get(['/', '/healthcheck'])
   async healthcheck() {
+    await this.dataSource.getRepository(User).find({ take: 1 });
+
     return { status: 'ok' };
   }
 }
