7884f78d5be60c8d079759ec89875c38153f8566
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-11-25 12:48:42 +0100

    minor changes

--- front-end/src/routes/show-hike/ShowHike.js
+++ front-end/src/routes/show-hike/ShowHike.js
@@ -78,23 +78,23 @@ const ShowHike = (props) => {
             <HTNavbar user={props.user} isLoggedIn={props.isLoggedIn} doLogOut={props.doLogOut} gotoLogin={gotoLogin} />
             <Grid style={{ marginTop: "105px", marginLeft: "auto", marginRight: "auto", marginBottom: "25px", height: "40vh" }} item lg={3}>
                 <Paper style={{ padding: "30px", height: "50vh" }}>
-                    <Grid item lg={12}>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                         <Typography variant="h4">General information</Typography>
                     </Grid>
                     <Divider textAlign="left" style={{ marginTop: "25px", marginBottom: "10px" }}>
                         <Chip label="Where to find" />
                     </Divider>
 
-                    <Grid item lg={12}>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                         {
                             !loading ?
-                                <Typography>Region: {hike.region==""?"N/A":hike.region}</Typography> :
+                                <Typography>Region: {hike.region == "" ? "N/A" : hike.region}</Typography> :
                                 <Skeleton variant='rectangular' height={20} width={200} style={{ marginBottom: "10px" }} />
                         }
                     </Grid>
-                    <Grid item lg={12}>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                         {
-                            !loading ? <Typography>Province: {hike.province==""?"N/A":hike.province}</Typography> :
+                            !loading ? <Typography>Province: {hike.province == "" ? "N/A" : hike.province}</Typography> :
                                 <Skeleton variant='rectangular' height={20} width={200} style={{ marginBottom: "10px" }} />
                         }
                     </Grid>
@@ -103,44 +103,48 @@ const ShowHike = (props) => {
                         <Chip label="About the hike" />
                     </Divider>
 
-                    <Grid item lg={12}>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                         {
-                            !loading ? <Typography>Length: {hike.length==""?"N/A":hike.length}km</Typography> :
+                            !loading ? <Typography>Length: {hike.length == "" ? "N/A" : hike.length}km</Typography> :
                                 <Skeleton variant='rectangular' height={20} width={200} style={{ marginBottom: "10px" }} />
                         }
                     </Grid>
-                    <Grid item lg={12}>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                         {
-                            !loading ? <Typography>Expected time: {hike.expectedTime==""?"N/A":hike.expectedTime} hours</Typography> :
+                            !loading ? <Typography>Expected time: {hike.expectedTime == "" ? "N/A" : hike.expectedTime} hours</Typography> :
                                 <Skeleton variant='rectangular' height={20} width={200} style={{ marginBottom: "10px" }} />
                         }
                     </Grid>
-                    <Grid item lg={12}>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                         {
-                            !loading ? <Typography>Ascent: {hike.ascent==""?"N/A":hike.ascent}m</Typography> :
+                            !loading ? <Typography>Ascent: {hike.ascent == "" ? "N/A" : hike.ascent}m</Typography> :
                                 <Skeleton variant='rectangular' height={20} width={200} style={{ marginBottom: "10px" }} />
                         }
                     </Grid>
                 </Paper>
             </Grid>
             <Grid style={{ marginTop: "105px", marginLeft: "auto", marginRight: "auto", marginBottom: "25px", height: "80vh", paddingLeft: "25px", paddingRight: "25px" }} item lg={6}>
-                <Grid item lg={12} style={{ display: "flex", justifyContent: "center", marginBottom: "15px" }}>
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12} style={{ display: "flex", justifyContent: "center", marginBottom: "15px" }}>
                     {
                         !loading ? <Typography variant="h2">{hike.title}</Typography> :
                             <Skeleton variant='rectangular' height={50} width={600} style={{ marginBottom: "10px" }} />
                     }
                 </Grid>
-                <Grid item lg={12}>
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                     <Divider>
                         <Difficulty loading={loading} diff={hike.difficulty} />
                     </Divider>
                 </Grid>
-                <Grid item lg={12} style={{ marginTop: "30px" }}>
-                    <Typography variant="h4">Some informations on this hike</Typography>
-                </Grid>
-                <Grid item lg={12}>
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12} style={{ marginTop: "30px" }}>
                     {
-                        !loading ? <Typography>{hike.description==""?"No description provided.":hike.description}</Typography> :
+                        !loading ?
+                            <Typography variant="h4">Some information on this hike</Typography>
+                            : <Typography variant="h4">Loading...</Typography>
+                    }
+                </Grid>
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                    {
+                        !loading ? <Typography>{hike.description == "" ? "No description provided." : hike.description}</Typography> :
                             <>
                                 <Skeleton variant='rectangular' height={20} width={400} style={{ marginBottom: "10px" }} />
                                 <Skeleton variant='rectangular' height={20} width={400} style={{ marginBottom: "10px" }} />
