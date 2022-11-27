6659af8a935450ba091128bbe29986d892ff34a6
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-11-27 20:42:31 +0100

    upd: navbar

--- front-end/src/components/HTNavbar/HTNavbar.js
+++ front-end/src/components/HTNavbar/HTNavbar.js
@@ -22,7 +22,9 @@ const bull = (
         component="span"
         sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
     >
-        •
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-square-fill" viewBox="0 0 16 16">
+            <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2z" />
+        </svg>   
     </Box>
 );
 
@@ -143,7 +145,7 @@ function HTNavbar(props) {
                                     <Button style={{ textTransform: "none", fontFamily: "Bakbak One, display", fontSize: "18px", marginRight: "12px" }}
                                         key={page.title}
                                         onClick={() => { handleCloseNavMenu(); navigate(page.URL) }}
-                                        sx={{ my: 2, color: 'white', display: 'block' }}
+                                        sx={{ my: 1, color: 'white', display: 'block' }}
                                     >
                                         {page.title}
                                     </Button>
@@ -237,8 +239,8 @@ function HTNavbar(props) {
                             {
                                 props.isLoggedIn ?
                                     <>
-                                        <div className='unselectable' style={{marginLeft: "12px", marginRight: "12px"}} key={settings[1]}>
-                                            <Typography color="black" fontFamily={"Bakbak One, display"} fontSize="20px" style={{display: "flex", justifyContent: "center"}}>
+                                        <div className='unselectable' style={{ marginLeft: "12px", marginRight: "12px" }} key={settings[1]}>
+                                            <Typography color="black" fontFamily={"Bakbak One, display"} fontSize="20px" style={{ display: "flex", justifyContent: "center" }}>
                                                 {props.user?.firstName} {props.user?.lastName}
                                             </Typography>
                                             <Typography color="black" fontFamily={"Bakbak One, display"} fontSize="16px">
