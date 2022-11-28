5f417b3678f6c6fe34f68c05e2f62e9e805f82b5
German Gorodnev <germangorodnev@gmail.com>
2022-11-28 11:45:46 +0100

    sonar

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,18 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ensureDir } from 'fs-extra';
+
+import { isTest, UPLOAD_PATH } from '@app/common';
 
 import { AppModule } from './app.module';
 import { prepareApp } from './prepare-app';
 
 async function bootstrap() {
+  if (isTest()) {
+    await ensureDir(UPLOAD_PATH, { mode: 0o777 });
+  }
+
   const port = process.env.PORT ? parseInt(process.env.PORT) : 3500;
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
