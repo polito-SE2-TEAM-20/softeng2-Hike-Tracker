6c87d240fdb2d1ad4332fac8de5e25830ae490b2
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2023-01-03 11:24:17 +0100

    feat: hike image is now shown

--- front-end/src/routes/show-hike/ShowHike.js
+++ front-end/src/routes/show-hike/ShowHike.js
@@ -15,6 +15,7 @@ import { UserRoles } from "../../lib/common/UserRoles";
 import { HikeWeatherByCode } from '../../lib/common/WeatherConditions'
 import { SvgIcon } from "@mui/material";
 import { MapContainer, TileLayer, Marker, ZoomControl, Polyline } from 'react-leaflet'
+import { APIURL } from "../../API/API.js";
 
 const Difficulty = (props) => {
     if (!props.loading) {
@@ -119,7 +120,28 @@ const ShowHike = (props) => {
 
     return (
         <Grid container flex style={{ minHeight: "100vh", height: "100%" }}>
-            <Grid style={{ marginTop: "50px", marginLeft: "auto", marginRight: "auto", marginBottom: "25px", height: "55vh" }} item lg={3}>
+            <Grid item>
+                <div style={{ display: "flex", justifyContent: "center", borderRadius: 12 }}>
+                    {
+                        !loading ?
+                            <img src={APIURL + hike.pictures[0]} alt={"landscape"} style={{ objectFit: "cover", width: "100vw", height: "400px" }} />
+                            :
+                            <></>
+                    }
+                </div>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ display: "flex", justifyContent: "center", marginTop: {xs: "-350px",md: "-200px"} }}>
+                {
+                    !loading ? <Typography variant="h2" sx={{ fontFamily: "Unbounded", textShadow: "#1a1a1a 0px 0 20px", color: "#fafafa", textAlign: "center" }}>{hike.title}</Typography> :
+                        <Skeleton variant='rectangular' height={50} width={600} style={{ marginBottom: "10px" }} />
+                }
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{marginTop: "12px"}}>
+                <Divider>
+                    <Difficulty loading={loading} diff={hike.difficulty} />
+                </Divider>
+            </Grid>
+            <Grid style={{ marginTop: "20px", marginLeft: "auto", marginRight: "auto", marginBottom: "25px", height: "fit-content" }} item lg={3}>
                 <Paper style={{ padding: "30px" }}>
                     <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                         <Typography variant="h4">General information</Typography>
@@ -218,7 +240,7 @@ const ShowHike = (props) => {
                                 onClick={() => {
                                     handleSaveForLater()
                                 }}>
-                                
+
                                 Save Hike For Later
                             </Fab>
                         }
@@ -226,28 +248,18 @@ const ShowHike = (props) => {
 
                 </Paper>
             </Grid>
-            <Grid style={{ marginTop: "50px", marginLeft: "auto", marginRight: "auto", marginBottom: "25px", height: "80vh", paddingLeft: "25px", paddingRight: "25px" }} item lg={6}>
-                <Grid item xs={12} sm={12} md={12} lg={12} xl={12} style={{ display: "flex", justifyContent: "center", marginBottom: "15px" }}>
-                    {
-                        !loading ? <Typography variant="h2" sx={{ fontFamily: "Unbounded" }}>{hike.title}</Typography> :
-                            <Skeleton variant='rectangular' height={50} width={600} style={{ marginBottom: "10px" }} />
-                    }
-                </Grid>
-                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                    <Divider>
-                        <Difficulty loading={loading} diff={hike.difficulty} />
-                    </Divider>
-                </Grid>
-                <Grid item xs={12} sm={12} md={12} lg={12} xl={12} style={{ marginTop: "30px" }}>
+            <Grid style={{ marginTop: "30px", marginLeft: "auto", marginRight: "auto", marginBottom: "25px", height: "80vh", paddingLeft: "25px", paddingRight: "25px" }} item lg={6}>
+
+                {/* <Grid item xs={12} sm={12} md={12} lg={12} xl={12} style={{ marginTop: "30px" }}>
                     {
                         !loading ?
                             <Typography variant="h4">Some information on this hike</Typography>
                             : <Typography variant="h4">Loading...</Typography>
                     }
-                </Grid>
+                </Grid> */}
                 <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                     {
-                        !loading ? <Typography>{hike.description == "" ? "No description provided." : hike.description}</Typography> :
+                        !loading ? <Typography>{hike.description === "" ? "No description provided." : hike.description}</Typography> :
                             <>
                                 <Skeleton variant='rectangular' height={20} width={400} style={{ marginBottom: "10px" }} />
                                 <Skeleton variant='rectangular' height={20} width={400} style={{ marginBottom: "10px" }} />
@@ -266,13 +278,13 @@ const ShowHike = (props) => {
                                     url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
                                 />
                                 <Marker
-                                        key={hike.id}
-                                        position={[hike?.positions[0][0], hike?.positions[0][1]]}>
-                                    </Marker>
-                                    <Polyline
-                                        pathOptions={{ fillColor: 'red', color: 'blue' }}
-                                        positions={hike?.positions}
-                                    />
+                                    key={hike.id}
+                                    position={[hike?.positions[0][0], hike?.positions[0][1]]}>
+                                </Marker>
+                                <Polyline
+                                    pathOptions={{ fillColor: 'red', color: 'blue' }}
+                                    positions={hike?.positions}
+                                />
                                 <ZoomControl position='bottomright' />
                             </MapContainer> :
                             <>
