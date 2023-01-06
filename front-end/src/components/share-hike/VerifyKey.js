74c816b32f6f9e0ff3f906c8025ac5cf3b3b359c
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2023-01-06 13:42:01 +0100

    fixed: no further access to terminated hikes by friends

--- front-end/src/components/share-hike/VerifyKey.js
+++ front-end/src/components/share-hike/VerifyKey.js
@@ -26,13 +26,26 @@ const VerifyKey = (props) => {
 
         apiSubmitCode()
             .then(() => {
-                setTimeout(() => {
-                    setMessage({ color: "green", message: "Access granted: " + response.hike.title })
-                }, 50);
-                setTimeout(() => {
-                    console.log(response)
-                    navigate("/friend-tracking/" + response.userId + "/" + response.hikeId + "/" + friendCode)
-                }, 2000);
+                if (response.status !== undefined && response.status === 422) {
+                    setSending(true)
+                    setMessage({ color: "red", message: "Access denied" })
+                    setTimeout(() => {
+                        setMessage({ color: "", message: "" })
+                    }, 2000);
+                    setTimeout(() => {
+                        setSending(false)
+                        setters.forEach(setter => setter(""))
+                        setIndex(0)
+                    }, 50);
+                } else {
+                    setTimeout(() => {
+                        setMessage({ color: "green", message: "Access granted: " + response.hike.title })
+                    }, 50);
+                    setTimeout(() => {
+                        console.log(response)
+                        navigate("/friend-tracking/" + response.userId + "/" + response.hikeId + "/" + friendCode)
+                    }, 2000);
+                }
             })
             .catch(() => {
                 setSending(true)
